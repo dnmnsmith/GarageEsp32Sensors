cd127f1866d7e6082b91c00c72308e81cb5d9e70
Duncan Smith <duncan@dandhome.net>
2021-06-06 21:46:55 +0100

    print output tidy

--- src/main.cpp
+++ src/main.cpp
@@ -59,21 +59,21 @@ void setup() {
   
   Serial.println("Hello");
 
-     // default settings
-    unsigned status = bme.begin(0x76, &Wire);  
+    // default settings
+  unsigned status = bme.begin(0x76, &Wire);  
 
-    // You can also pass in a Wire library object like &Wire2
-    // status = bme.begin(0x76, &Wire2)
-    if (!status) {
-        Serial.println("Could not find a valid BME280 sensor, check wiring, address, sensor ID!");
-        Serial.print("SensorID was: 0x"); Serial.println(bme.sensorID(),16);
-        Serial.print("        ID of 0xFF probably means a bad address, a BMP 180 or BMP 085\n");
-        Serial.print("   ID of 0x56-0x58 represents a BMP 280,\n");
-        Serial.print("        ID of 0x60 represents a BME 280.\n");
-        Serial.print("        ID of 0x61 represents a BME 680.\n");
-        while (1) delay(10);
-    }
- 
+  // You can also pass in a Wire library object like &Wire2
+  // status = bme.begin(0x76, &Wire2)
+  if (!status) {
+      Serial.println("Could not find a valid BME280 sensor, check wiring, address, sensor ID!");
+      Serial.print("SensorID was: 0x"); Serial.println(bme.sensorID(),16);
+      Serial.print("        ID of 0xFF probably means a bad address, a BMP 180 or BMP 085\n");
+      Serial.print("   ID of 0x56-0x58 represents a BMP 280,\n");
+      Serial.print("        ID of 0x60 represents a BME 280.\n");
+      Serial.print("        ID of 0x61 represents a BME 680.\n");
+      while (1) delay(10);
+  }
+
   // Start up the library
   sensors.begin();
   
@@ -93,17 +93,13 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   sensors.requestTemperatures();
-
-  printTemperature(insideThermometer);
-  Serial.println();
 
   display.outsideTemp( bme.readTemperature() );
   display.insideTemp( sensors.getTempC(insideThermometer) );
   display.pressure( bme.readPressure() / 100.0F );
   display.humidity( bme.readHumidity() );
-  display.rssi( -110.0 );
+  display.rssi( (float)WiFi.RSSI() );
 
   delay(5000);
 }
